db193e73700f318253dc937622221e1294f3ce33
lemarco <lemarc.corp@gmail.com>
2020-06-11 09:46:46 +0300

    delete tests from index

--- src/index.ts
+++ src/index.ts
@@ -8,26 +8,3 @@ const insertion_array: Array<number> = [5, 2, 4, 6, 1, 3, 0];
 const selection_array: Array<number> = [5, 2, 4, 6, 1, 3, 0];
 const ascending_lambda = (a: any, b: any) => a - b;
 const discending_lambda = (a: any, b: any) => b - a;
-
-console.dir(insertion_array);
-console.dir(insertion_array.bubble_sort(discending_lambda));
-console.dir(insertion_array);
-
-// console.dir(
-//     `insertion_sort([${insertion_array}], discending_lambda) => returns ${insertion_sort(
-//         insertion_array,
-//         discending_lambda,
-//     )}`,
-// );
-// console.dir(
-//     `selection_sort([${selection_array}], discending_lambda) => returns ${selection_sort(
-//         selection_array,
-//         discending_lambda,
-//     )}`,
-// );
-// console.dir(
-//     `merge_sort([${merge_array}], ascending_lambda) => returns ${merge_array.merge_sort(
-//         merge_array,
-//         discending_lambda,
-//     )}`,
-// );
